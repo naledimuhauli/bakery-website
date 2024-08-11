59076d8a5a49b1c2db883a723563d28c5985b7ab
siphokazi <naledimuhauli@gmail.com>
2024-08-11 20:40:16 +0200

    styled about us section

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -21,19 +21,18 @@ const AboutUs = () => {
                 <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
                     <div className="col-lg-7 p-3 p-lg-5 pt-lg-3">
                         <h1 className="display-4 fw-bold lh-1 text-body-emphasis">About Dreamy Bakes</h1>
-                        <h5 className='our-story'>Our Story</h5>
+                        <h5 className='title'>Our Story</h5>
                         <p className="lead">{details[0].ourStory}</p>
-                        <h5>Our Mission</h5>
-                        <p>{details[0].ourMission}</p>
-                        <h5>Our Values</h5>
+                        <h5 className='title'>Our Mission</h5>
+                        <p className="lead">{details[0].ourMission}</p>
+                        <h5 className='title'>Our Values</h5>
                         <ul>
                             {details[0].ourValues.map((detail, index) => (
-                                <li key={index}>{detail}</li>
+                                <li key={index} className="lead">{detail}</li>
                             ))}
                         </ul>
                         <div className="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
-                            <button type="button" className="btn btn-primary btn-lg px-4 me-md-2 fw-bold">Learn More</button>
-                            <button type="button" className="btn btn-outline-secondary btn-lg px-4">Contact Us</button>
+                            <button type="button" className="btn btn-outline-secondary btn-lg px-4 contact">Contact Us</button>
                         </div>
                     </div>
                     <div className="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
